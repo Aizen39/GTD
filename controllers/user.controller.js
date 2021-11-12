928c07f5d07f5dd46bf11b2b9104c54efbaa367d
Anthony HO <aho@pageup.fr>
2021-11-12 11:00:30 +0100

    GET all users except password

---
+++ controllers/user.controller.js
@@ -0,0 +1,7 @@
+const UserModel = require('../models/user.model');
+const ObjectfID = require('mongoose').Types.ObjectId;
+
+module.exports.getAllUsers = async (req,res) => {
+    const users = await UserModel.find().select();
+    res.status(200).json(users);
+}
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth.controller.Js');
+const userController = require ('../controllers/user.controller');
 
-//ajout d'un utilisateur
+
+//auth
 router.post('/register', authController.signUp);
+
+router.get('/', userController.getAllUsers);
 
 module.exports = router;
