8dffd9b34f8dc1b20358c0f082334831fc140568
Anthony HO <aho@pageup.fr>
2021-11-12 11:01:35 +0100

    GET all users except password fix bug

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,6 +2,6 @@ const UserModel = require('../models/user.model');
 const ObjectfID = require('mongoose').Types.ObjectId;
 
 module.exports.getAllUsers = async (req,res) => {
-    const users = await UserModel.find().select();
+    const users = await UserModel.find().select('-password');
     res.status(200).json(users);
 }
