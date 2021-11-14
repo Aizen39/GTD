7477c4ed2887ea234610b1e51d34b87d708730e5
Anthony HO <aho@pageup.fr>
2021-11-14 17:00:46 +0100

    token control

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 //framework express
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
 //accès aux variables d'environnement
 require('dotenv').config({path: './config/.env'})
 require('./config/db');
+const {checkUser} = require('./middleware/auth.middleware')
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+//jwt
+app.get('*', checkUser);
 
 //routes 
 app.use('/api/user', userRoutes);
