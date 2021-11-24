64138021c30fea94d83d01265881801fd93f29d6
Anthony HO <aho@pageup.fr>
2021-11-24 23:46:08 +0100

    uploads images

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,26 +1,54 @@
-const PostModel = require('../models/post.model');
-const UserModel = require('../models/user.model');
-const ObjectID = require('mongoose').Types.ObjectId;
+const postModel = require("../models/post.model");
+const PostModel = require("../models/post.model");
+const UserModel = require("../models/user.model");
+const { uploadErrors } = require("../utils/errors.utils");
+const ObjectID = require("mongoose").Types.ObjectId;
+const fs = require("fs");
+const { promisify } = require("util");
+const pipeline = promisify(require("stream").pipeline);
 
 module.exports.readPost = (req, res) => {
     PostModel.find((err, docs) => {
-        if (!err)
-            res.send(docs);
-        else
-            console.log('error to get data' + err);
-    }).sort({ createdAt: -1 }); //trier au plus récent
+        if (!err) res.send(docs);
+        else console.log("Error to get data : " + err);
+    }).sort({ createdAt: -1 });
 };
 
 module.exports.createPost = async (req, res) => {
-    const newPost = new PostModel({
+    let fileName;
+
+    if (req.file !== null) {
+        try {
+            if (
+                req.file.detectedMimeType != "image/jpg" &&
+                req.file.detectedMimeType != "image/png" &&
+                req.file.detectedMimeType != "image/jpeg"
+            )
+                throw Error("invalid file");
+
+            if (req.file.size > 500000) throw Error("max size");
+        } catch (err) {
+            const errors = uploadErrors(err);
+            return res.status(201).json({ errors });
+        }
+        fileName = req.body.posterId + Date.now() + ".jpg";
+
+        await pipeline(
+            req.file.stream,
+            fs.createWriteStream(
+                `${__dirname}/../client/public/upload/posts/${fileName}`
+            )
+        );
+    }
+
+    const newPost = new postModel({
         posterId: req.body.posterId,
         message: req.body.message,
+        picture: req.file !== null ? "./upload/posts/" + fileName : "",
         video: req.body.video,
         likers: [],
         comments: [],
     });
-
-
 
     try {
         const post = await newPost.save();
@@ -53,15 +81,10 @@ module.exports.deletePost = (req, res) => {
     if (!ObjectID.isValid(req.params.id))
         return res.status(400).send("ID unknown : " + req.params.id);
 
-    PostModel.findByIdAndRemove(
-        req.params.id,
-        (err, docs) => {
-            if (!err)
-                res.send(docs);
-            else
-                console.log('Delete error:' + err);
-        }
-    );
+    PostModel.findByIdAndRemove(req.params.id, (err, docs) => {
+        if (!err) res.send(docs);
+        else console.log("Delete error : " + err);
+    });
 };
 
 module.exports.likePost = async (req, res) => {
@@ -75,12 +98,10 @@ module.exports.likePost = async (req, res) => {
                 $addToSet: { likers: req.body.id },
             },
             { new: true },
-
             (err, docs) => {
                 if (err) return res.status(400).send(err);
             }
         );
-
         await UserModel.findByIdAndUpdate(
             req.body.id,
             {
@@ -108,12 +129,10 @@ module.exports.unlikePost = async (req, res) => {
                 $pull: { likers: req.body.id },
             },
             { new: true },
-
             (err, docs) => {
                 if (err) return res.status(400).send(err);
             }
         );
-
         await UserModel.findByIdAndUpdate(
             req.body.id,
             {
@@ -145,7 +164,6 @@ module.exports.commentPost = (req, res) => {
                         text: req.body.text,
                         timestamp: new Date().getTime(),
                     },
-
                 },
             },
             { new: true },
@@ -155,9 +173,8 @@ module.exports.commentPost = (req, res) => {
             }
         );
     } catch (err) {
-        return res.status(400);
+        return res.status(400).send(err);
     }
-
 };
 
 module.exports.editCommentPost = (req, res) => {
@@ -165,21 +182,19 @@ module.exports.editCommentPost = (req, res) => {
         return res.status(400).send("ID unknown : " + req.params.id);
 
     try {
-        return PostModel.findById(
-            req.params.id,
-            (err, docs) => {
-                const theComment = docs.comments.find((comment) =>
-                    comment._id.equals(req.body.commentId)
-                )
-                if (!theComment) return res.status(404).send('COmment not found')
-                theComment.text = req.body.text;
+        return PostModel.findById(req.params.id, (err, docs) => {
+            const theComment = docs.comments.find((comment) =>
+                comment._id.equals(req.body.commentId)
+            );
 
-                return docs.save((err) => {
-                    if (!err) return res.status(200).send(docs);
-                    return res.status(500).send(err);
-                })
-            }
-        )
+            if (!theComment) return res.status(404).send("Comment not found");
+            theComment.text = req.body.text;
+
+            return docs.save((err) => {
+                if (!err) return res.status(200).send(docs);
+                return res.status(500).send(err);
+            });
+        });
     } catch (err) {
         return res.status(400).send(err);
     }
@@ -196,20 +211,16 @@ module.exports.deleteCommentPost = (req, res) => {
                 $pull: {
                     comments: {
                         _id: req.body.commentId,
-
-                    }
-                }
+                    },
+                },
             },
             { new: true },
             (err, docs) => {
                 if (!err) return res.send(docs);
-                else return res.status(400).send(err)
+                else return res.status(400).send(err);
             }
-
         );
     } catch (err) {
         return res.status(400).send(err);
     }
 };
-
-
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
 const postController = require('../controllers/post.controller');
+const multer = require('multer');
+const upload = multer();
 
 //CRUD d'un post
 router.get('/', postController.readPost);
 router.post('/', postController.createPost);
 router.put('/:id', postController.updatePost);
 router.delete('/:id', postController.deletePost);
+router.post('/', upload.single('file'), postController.createPost);
 
 //likes
 router.patch('/like-post/:id', postController.likePost);
